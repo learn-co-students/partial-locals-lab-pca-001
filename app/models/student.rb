acd94e705abc5a980ed7f9353e666fece4927490
Aaron Wilson <adub65@gmail.com>
2020-06-18 10:56:50 -0400

    partials and locals, search added in model and controller

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,8 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(string)
+    all.where("name LIKE ?", "%#{string}%")
+  end
 end
